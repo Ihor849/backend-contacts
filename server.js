fc4b246e1f93ed63e254a86e4cd3dc6bb4e3d9f9
Ihor849 <Marchelo849@outlook.com>
2023-07-12 22:43:33 +0300

    dotenv

--- server.js
+++ server.js
@@ -3,8 +3,7 @@
 const mongoose=require("mongoose");
 const app = require('./app');
 const {DB_HOST}=process.env;
-console.log(process.env.DB_HOST);
-// const DB_HOST = "mongodb+srv://Ihor:ptQvw*8PdgWQsZH@cluster0.qbxqqlz.mongodb.net/contacts_friends?retryWrites=true&w=majority";
+
 
 mongoose.set("strictQuery",true);
 
